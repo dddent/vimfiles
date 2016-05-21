92728558930c76e06ef603a4cca76477396e8113
root <heuschmann.d@gmail.com>
2016-05-21 15:05:26 +0200

    added plugin installation sh

---
+++ install_plugins.sh
@@ -0,0 +1,14 @@
+rm -r ~/.vim/bundle/*
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/tmhedberg/matchit.git
+git clone https://github.com/honza/vim-snippets.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
